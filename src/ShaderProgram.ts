3722df65251796301d2637471e6976813d2e0473
Italo Di Renzo <italodirenzo93@gmail.com>
2022-04-11 20:43:16 -0400

    Switch to using uniform buffer

--- src/ShaderProgram.ts
+++ src/ShaderProgram.ts
@@ -1,11 +1,18 @@
 import { ReadonlyMat4 } from "gl-matrix";
 import { minifyShaderCode } from "./helpers";
 
-interface UniformBufferMVP {
-    uMatProj: ReadonlyMat4;
-    uMatView: ReadonlyMat4;
-    uMatModel: ReadonlyMat4;
-}
+const uniformNames = ["u_MatProj", "u_MatView", "u_MatModel"] as const;
+
+export type UniformBufferMVP = Record<
+    typeof uniformNames[number],
+    ReadonlyMat4
+>;
+
+// interface UniformBufferMVP {
+//     uMatProj: ReadonlyMat4;
+//     uMatView: ReadonlyMat4;
+//     uMatModel: ReadonlyMat4;
+// }
 
 // Vertex shader
 const vertexShaderSource = `#version 300 es
@@ -16,15 +23,11 @@ layout(location=1) in vec2 a_TextureCoord;
 
 out vec2 textureCoord;
 
-uniform mat4 u_MatProj;
-uniform mat4 u_MatView;
-uniform mat4 u_MatModel;
-
-// uniform MVP {
-//     mat4 uMatProj;
-//     mat4 uMatView;
-//     mat4 uMatModel;
-// };
+uniform MVP {
+    mat4 u_MatProj;
+    mat4 u_MatView;
+    mat4 u_MatModel;
+};
 
 void main() {
     gl_Position = u_MatProj * u_MatView * u_MatModel * vec4(a_Position, 1);
@@ -35,7 +38,7 @@ void main() {
 const fragmentShaderSource = `#version 300 es
 #pragma vscode_glsllint_stage: frag
 
-precision mediump float;
+precision highp float;
 
 in vec2 textureCoord;
 out vec4 fragColor;
@@ -46,18 +49,37 @@ void main() {
     fragColor = texture(u_Sampler, textureCoord);
 }`;
 
+export interface UniformVariableInfo {
+    index: number;
+    offset: number;
+}
+
 export class ShaderProgram {
     private static s_program: WebGLProgram;
 
     private static s_aPosition: number;
     private static s_aTextureCoord: number;
 
-    private static s_uMatProj: WebGLUniformLocation;
-    private static s_uMatView: WebGLUniformLocation;
-    private static s_uMatModel: WebGLUniformLocation;
     private static s_uSampler: WebGLUniformLocation;
+    private static s_uniformBuffer: WebGLBuffer;
 
-    // private static s_uniformBuffer: WebGLBuffer;
+    private static s_uniformInfo: Record<
+        typeof uniformNames[number],
+        UniformVariableInfo
+    > = {
+        u_MatProj: {
+            index: 0,
+            offset: 0,
+        },
+        u_MatView: {
+            index: 0,
+            offset: 0,
+        },
+        u_MatModel: {
+            index: 0,
+            offset: 0,
+        },
+    };
 
     constructor(private readonly gl: WebGL2RenderingContext) {
         // If the program hasn't been compiled and linked already do it now
@@ -74,7 +96,7 @@ export class ShaderProgram {
         this.gl.useProgram(ShaderProgram.s_program);
     }
 
-    setVertexData(buffer: WebGLBuffer | null) {
+    setVertexData(buffer: WebGLBuffer | null): void {
         if (buffer) {
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
             this.gl.vertexAttribPointer(
@@ -86,13 +108,13 @@ export class ShaderProgram {
                 0
             );
             this.gl.enableVertexAttribArray(ShaderProgram.s_aPosition);
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+            // this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
         } else {
             this.gl.disableVertexAttribArray(ShaderProgram.s_aPosition);
         }
     }
 
-    setTextureCoordinates(buffer: WebGLBuffer | null) {
+    setTextureCoordinates(buffer: WebGLBuffer | null): void {
         if (buffer) {
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
             this.gl.vertexAttribPointer(
@@ -104,22 +126,22 @@ export class ShaderProgram {
                 0
             );
             this.gl.enableVertexAttribArray(ShaderProgram.s_aTextureCoord);
-            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
+            // this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
         } else {
             this.gl.disableVertexAttribArray(ShaderProgram.s_aTextureCoord);
         }
     }
 
     setProjectionMatrix(matrix: ReadonlyMat4): void {
-        this.gl.uniformMatrix4fv(ShaderProgram.s_uMatProj, false, matrix);
+        ShaderProgram.setUniformMatrix(this.gl, "u_MatProj", matrix);
     }
 
     setViewMatrix(matrix: ReadonlyMat4): void {
-        this.gl.uniformMatrix4fv(ShaderProgram.s_uMatView, false, matrix);
+        ShaderProgram.setUniformMatrix(this.gl, "u_MatView", matrix);
     }
 
     setModelMatrix(matrix: ReadonlyMat4): void {
-        this.gl.uniformMatrix4fv(ShaderProgram.s_uMatModel, false, matrix);
+        ShaderProgram.setUniformMatrix(this.gl, "u_MatModel", matrix);
     }
 
     setTexture(texture: WebGLTexture): void {
@@ -173,47 +195,74 @@ export class ShaderProgram {
         ShaderProgram.s_aPosition = 0;
         ShaderProgram.s_aTextureCoord = 1;
 
-        ShaderProgram.s_uMatProj = gl.getUniformLocation(
-            ShaderProgram.s_program,
-            "u_MatProj"
-        );
-
-        ShaderProgram.s_uMatView = gl.getUniformLocation(
-            ShaderProgram.s_program,
-            "u_MatView"
-        );
-
-        ShaderProgram.s_uMatModel = gl.getUniformLocation(
-            ShaderProgram.s_program,
-            "u_MatModel"
-        );
-
         ShaderProgram.s_uSampler = gl.getUniformLocation(
             ShaderProgram.s_program,
             "u_Sampler"
         );
 
         // Uniform buffer
-        // {
-        //     const blockIndex = gl.getUniformBlockIndex(
-        //         ShaderProgram.s_program,
-        //         "MVP"
-        //     );
-        //     const blockSize = gl.getActiveUniformBlockParameter(
-        //         ShaderProgram.s_program,
-        //         blockIndex,
-        //         gl.UNIFORM_BLOCK_DATA_SIZE
-        //     );
+        {
+            const blockIndex = gl.getUniformBlockIndex(
+                ShaderProgram.s_program,
+                "MVP"
+            );
 
-        //     const uniformBuffer = gl.createBuffer();
-        //     gl.bindBuffer(gl.UNIFORM_BUFFER, uniformBuffer);
-        //     gl.bufferData(gl.UNIFORM_BUFFER, blockSize, gl.DYNAMIC_DRAW);
-        //     gl.bindBuffer(gl.UNIFORM_BUFFER, null);
-        //     // Bind the buffer to a binding point
-        //     // Think of it as storing the buffer into a special UBO ArrayList
-        //     // The second argument is the index you want to store your Uniform Buffer in
-        //     // Let's say you have 2 unique UBO, you'll store the first one in index 0 and the second one in index 1
-        //     gl.bindBufferBase(gl.UNIFORM_BUFFER, 0, uniformBuffer);
-        // }
+            gl.uniformBlockBinding(ShaderProgram.s_program, blockIndex, 0);
+
+            const blockSize = gl.getActiveUniformBlockParameter(
+                ShaderProgram.s_program,
+                blockIndex,
+                gl.UNIFORM_BLOCK_DATA_SIZE
+            );
+
+            ShaderProgram.s_uniformBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.UNIFORM_BUFFER, ShaderProgram.s_uniformBuffer);
+            gl.bufferData(gl.UNIFORM_BUFFER, blockSize, gl.DYNAMIC_DRAW);
+            gl.bindBuffer(gl.UNIFORM_BUFFER, null);
+
+            // Bind the buffer to a binding point
+            // Think of it as storing the buffer into a special UBO ArrayList
+            // The second argument is the index you want to store your Uniform Buffer in
+            // Let's say you have 2 unique UBO, you'll store the first one in index 0 and the second one in index 1
+            gl.bindBufferBase(
+                gl.UNIFORM_BUFFER,
+                blockIndex,
+                ShaderProgram.s_uniformBuffer
+            );
+
+            const uniformIndices = gl.getUniformIndices(
+                ShaderProgram.s_program,
+                uniformNames
+            );
+
+            const uniformOffsets = gl.getActiveUniforms(
+                ShaderProgram.s_program,
+                uniformIndices,
+                gl.UNIFORM_OFFSET
+            );
+
+            uniformNames.forEach((name, index) => {
+                ShaderProgram.s_uniformInfo[name].index = uniformIndices[index];
+                ShaderProgram.s_uniformInfo[name].offset =
+                    uniformOffsets[index];
+            });
+        }
+    }
+
+    private static setUniformMatrix(
+        gl: WebGL2RenderingContext,
+        name: typeof uniformNames[number],
+        matrix: ReadonlyMat4
+    ): void {
+        // console.log(name, "offset", ShaderProgram.s_uniformInfo[name].offset);
+        gl.bindBuffer(gl.UNIFORM_BUFFER, ShaderProgram.s_uniformBuffer);
+
+        gl.bufferSubData(
+            gl.UNIFORM_BUFFER,
+            ShaderProgram.s_uniformInfo[name].offset,
+            new Float32Array(matrix)
+        );
+
+        gl.bindBuffer(gl.UNIFORM_BUFFER, null);
     }
 }
